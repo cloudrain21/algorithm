32b12027569fb12562f0ab6c9fb0a0ab95d6ff0a
cloudrain21 <cloudrain21@gmail.com>
2022-09-20 09:43:37 +0900

    algorithm - baekjoon - set and map

--- baekjoon/12.set_and_map/11478.go
+++ baekjoon/12.set_and_map/11478.go
@@ -1,4 +1,20 @@
 package main
 
+import "fmt"
+
 func main() {
+	var s string
+	fmt.Scan(&s)
+
+	m := map[string]struct{}{}
+
+	l := len(s)
+	for i := 0; i < l; i++ {
+		for j := i + 1; j <= l; j++ {
+			b := i
+			e := j
+			m[s[b:e]] = struct{}{}
+		}
+	}
+	fmt.Println(len(m))
 }
